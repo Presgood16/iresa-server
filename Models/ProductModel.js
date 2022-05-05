5819997503952b5062e7afebb514e6741eb0f8a3
Taiwo <adeyemitaiwo61@gmail.com>
2022-05-05 12:06:51 +0100

    make it better

--- Models/ProductModel.js
+++ Models/ProductModel.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 
 const reviewSchema = mongoose.Schema(
   {
-    name: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     rating: { type: Number, required: true },
     comment: { type: String, required: true },
     user: {
